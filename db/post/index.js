b7534887006c52b3b70cfa46d2ce3cd1c6947e24
Judah <jk5445@nyu.edu>
2019-08-14 10:35:32 -0400

    implement /apiv1/post

--- db/post/index.js
+++ db/post/index.js
@@ -1,3 +1,5 @@
+const db = require('./query');
+
 //Posts
 //subcomponents of conversations
 //for now users can not edit or delete posts
@@ -6,11 +8,34 @@ modules.exports = app => {
     //create post
     //auth
     app.post('/apiv1/post', (request, response) => {
-        //TODO: implement method
+        const user_id = request.body.user_id;
+        const convo_id = request.body.convo_id;
+        const post = request.body.post;
+
+        let authorized = false;
+        try {
+            authorized = await db.authorize(convo_id, user_id);
+        } catch(err) {
+            throw err;
+        }
+
+        if(authorized) {
+            try {
+                await db.createPost(convo_id, user_id, post);
+            } catch (err) {
+                throw err;
+            }
+
+            response.status(201).end();
+        }
     });
 
     //get post
+    //secondary priority
+    
     app.get('/apiv1/post/:convo_id/:postNumber', (request, response) => {
         //TODO: implement method
+        response.end();
     });
+    
 }
