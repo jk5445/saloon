66c62139ab3a6d7ed40bc56881740c5255b00828
Judah <jk5445@nyu.edu>
2019-08-13 22:17:32 -0400

    index file shoul call authorize function

--- db/post/query.js
+++ db/post/query.js
@@ -1,41 +1,32 @@
 const db = require('../queries');
+const user = require('../user/query');
 
 //a user needs to be part of a conversation to post
+//this check should happen at server level
 const createPost = async (convo_id, contributor_id, post) => {
 	//with auth contributor_id will come from JWT
 	/*const {post, contributor_id, convo_id} = request.body;
 	*/
 
-	const authorized = false;
-	try {
-		authorized = await authorize(convo_id, contributor_id);
-	}
-	catch(error){
-		throw error;
-	}
-
-	if (authorized) {
-		db.query(
-			'INSERT INTO post (convo_id, post, contributor_id) VALUES ($1, $2, $3)',
-			[convo_id, post, contributor_id],
-			(error, _results) => {
-			  	if (error){
-					throw error;
-				}
-				response.status(201).send();
-				db.query(
-					'UPDATE convo SET posts = posts + 1 WHERE convo_id = $1',
-					[convo_id],
-					(error, _results) => {
-						if (error) {
-							throw error;
-						}
-						return;
-					}
-				);
+	db.query(
+		'INSERT INTO post (convo_id,contributor_id, post) VALUES ($1, $2, $3)',
+		[convo_id, contributor_id,  post],
+		(error, _results) => {
+		  	if (error){
+				throw error;
 			}
-		);
-	}
+			db.query(
+				'UPDATE convo SET posts = posts + 1 WHERE convo_id = $1',
+				[convo_id],
+				(error, _results) => {
+					if (error) {
+						throw error;
+					}
+					return;
+				}
+			);
+		}
+	);
 }
 
 const getPosts = async (convo_id) => {
@@ -46,31 +37,30 @@ const getPosts = async (convo_id) => {
 			if (error) {
 				throw error;
 			}
-			var i;
+			let i;
 			for (i = 0; i < results.rowCount; i++) {
 				post = {};
 				post.post = results.rows[i]['post'];
 				post.created_at = results.rows[i]['created_at'];
-				db.query(
-					'SELECT user_name FROM user WHERE user_id = $1',
-					[results.rows[i]['contributor_id']],
-					(error, result) => {
-						if (error) {
-							throw error;
-						}
-						post.contributor = result.rows[0]['user_name'];
-						posts.push(post);
-					}
-				);
+
+				try {
+					const user_id = results.rows[i]['user_id']
+					post.contributor = await user.getUserName(user_id);
+				} catch(error) {
+					throw error;
+				}
+
+				posts.push(post);
 			}
 			return posts;
 		}
 	);
 }
 
+//call directly from server
 const authorize = async (convo_id, contributor_id) => {
 	//verify
-	var authorized = false; 
+	let authorized = false; 
 	db.query(
 		'SELECT 1 FROM contributor WHERE convo_id = $1 AND contributor_id = $2',
 		[convo_id, contributor_id],
@@ -79,10 +69,10 @@ const authorize = async (convo_id, contributor_id) => {
 				throw error;
 			}
 			//true if record is found
-			authorized = await (results.rowCount > 0);
+			authorized = (results.rowCount > 0);
 			return authorized;
 		}
 	);
 }
 
-modules.exports(createPost, getPosts);
+modules.exports(createPost, getPosts, authorize);
