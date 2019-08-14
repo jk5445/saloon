07ac8a40aeb1aa6f4976937e98db554dd150c48a
Judah <jk5445@nyu.edu>
2019-08-13 21:45:05 -0400

    implement createUser, logIn, getUserName

--- db/user/query.js
+++ db/user/query.js
@@ -1,45 +1,76 @@
 const db = require('../queries');
 const bcrypt = require('bcrypt');
 
-const createUser = (request, response) => {
-	const {userName, firstName, lastName, email, password} = request.body;
-
-  	bcrypt.hash(password, saltRounds, (error, hash) => {
-		if (error){
-			throw error;
-		}
-		db.query(
-		  'INSERT INTO users (user_name, first_name, last_name, email, password_hash) VALUES ($1, $2, $3, $4, $5) RETURNING user_ID',
-		  [userName, firstName, lastName, email, hash],
-		  (error, results) => {
-	    	if (error) {
-	    		throw error;
-	    	}
-	    	response.status(201).send(results.rows[0]);
-	  	  }
-	  	);
-	});
+const createUser = async (userName, firstName, lastName, email, password) => {
+	
+	const hash = await bcrypt.hash(password, saltRounds);
+	if (error){
+		throw error;
+	}
+	db.query(
+		'INSERT INTO users (user_name, first_name, last_name, email, password_hash) VALUES ($1, $2, $3, $4, $5) RETURNING user_id',
+		[userName, firstName, lastName, email, hash],
+		(error, results) => {
+	   		if (error) {
+	   			throw error;
+			}
+			
+			var user = {};
+			user.user_id = results.rows[0]['user_id'];
+	   		return user;
+	 	}
+	);
 }
 
 //email and password in body
 //issue token
-const logIn = (request, response) => {
-
+const logIn = async (email, password) => {
+	db.query(
+		'SELECT password_hash FROM user WHERE email=$1',
+		[email],
+		(error, results) => {
+			if (error) {
+				throw error;
+			}
+			const hash = results.rows[0]['password_hash'];
+			const valid = await bcrypt.compare(password, hash)
+			return valid;
+		}
+	)
 }
 
-const getUserByID = (request, response) => {
-	const id = parseInt(request.params.id);
-	//implement auth
-	//id will be extracted form JWT
+//TODO: add user history to user object
+const getUserByID = async (user_id) => {
 
 	db.query(
 	  'SELECT user_name, first_name, last_name, email FROM users WHERE user_ID = $1',
-	  [id],
+	  [user_id],
+	  (error, results) => {
+		if (error) {
+      		throw error;
+		}
+		
+		var user = {};
+		user.user_name = results.rows[0]['user_name'];
+		user.first_name = results.rows[0]['first_name'];
+		user.last_name = results.rows[0]['last_name'];
+		user.email = results.rows[0]['email'];
+    	return user;
+  	  }
+  	);
+}
+
+const getUserName = async (user_id) => {
+	db.query(
+	  'SELECT user_name FROM users WHERE user_ID = $1',
+	  [user_id],
 	  (error, results) => {
 		if (error) {
       		throw error;
     	}
-    	response.status(200).json(results.rows[0]);
+    	return results.rows[0]['user_name'];
   	  }
   	);
 }
+
+modeuls.exports(createUser, logIn, getUserName);
