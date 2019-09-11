8d1fed60b4a464704b9bfb6bead7b72755a4520d
Jack Hedaya <jackehedaya@gmail.com>
2019-09-11 15:32:14 -0400

    send token in json form

--- db/auth.js
+++ db/auth.js
@@ -18,7 +18,7 @@ function createToken (user_id, response){
 			console.log("Error creating token", err)
 			return response.status(400).send("Error creating token")
 		}
-		return response.send(token)
+		return response.send(JSON.stringify({ token }))
 	})
 }
 
