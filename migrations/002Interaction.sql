32f60214477a0204c49e3cd8ed3356a9da15a519
Judah <jk5445@nyu.edu>
2019-09-17 21:38:43 -0400

    serial to uuid

--- migrations/002Interaction.sql
+++ migrations/002Interaction.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS convo_vote (
-    vote_id serial PRIMARY KEY,
-    convo_id integer REFERENCES convo (convo_id) NOT NULL,
-    user_id integer REFERENCES users (user_id) NOT NULL,
+    vote_id UUID DEFAULT uuid_generate_v1() PRIMARY KEY,
+    convo_id UUID REFERENCES convo (convo_id) NOT NULL,
+    user_id UUID REFERENCES users (user_id) NOT NULL,
     vote integer NOT NULL,
     vote_at timestamp WITH time zone NOT NULL DEFAULT NOW()
 );
@@ -9,16 +9,16 @@ CREATE TABLE IF NOT EXISTS convo_vote (
 CREATE TABLE IF NOT EXISTS comment (
     comment_id serial PRIMARY KEY,
     comment text NOT NULL,
-    convo_id integer REFERENCES convo (convo_id) NOT NULL,
-    user_id integer REFERENCES users (user_id) NOT NULL,
+    convo_id UUID REFERENCES convo (convo_id) NOT NULL,
+    user_id UUID REFERENCES users (user_id) NOT NULL,
     comment_at timestamp WITH TIME ZONE NOT NULL DEFAULT NOW(),
     votes integer NOT NULL DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS comment_vote (
-    vote_id serial PRIMARY KEY,
-    comment_id integer REFERENCES comment (comment_id) NOT NULL,
-    user_id integer REFERENCES users (user_id) NOT NULL,
+    vote_id UUID DEFAULT uuid_generate_v1() PRIMARY KEY,
+    comment_id UUID REFERENCES comment (comment_id) NOT NULL,
+    user_id UUID REFERENCES users (user_id) NOT NULL,
     vote integer NOT NULL,
     vote_at timestamp WITH time zone NOT NULL DEFAULT NOW()
 );
