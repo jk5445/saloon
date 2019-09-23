d2fbd4022e28e2bae56fb9a24042edeb64bd717d
Judah <jk5445@nyu.edu>
2019-09-23 12:12:33 -0400

    clean code

--- db/feed/query.js
+++ db/feed/query.js
@@ -11,100 +11,20 @@ module.exports = {
 
 function getFeed(batch, serve) {
     const query = "SELECT * FROM convo ORDER BY votes DESC"
-    db.query(
-        query, [], (error, results) => {
-            if(error){
-                console.error('select feed failed', error)
-                return serve (true, "select feed failed")
-            } else if (results.rowCount < 1) {
-                console.error('Empty feed')
-                return serve (true, 'Empty feed')
-            }
-
-            convos = []
-
-            let i = (batch - 1) * 10
-            let count = i
-            const limit = (results.rowCount < i + 10) ? results.rowCount : 10 + i
-            for(i; i < limit; i++){
-                let convo = results.rows[i]
-                const last_post = moment.utc(convo.last_post_at, moment.ISO_8601)
-                convo.age = moment(last_post).fromNow()
-
-                getContributors(convo.convo_id, (error, response) => {
-                    if(error){
-                        return serve (error, response)
-                    }
-                    convo.contributors = stringifyContributors(response)
-                    convo.contributorCount = response.length
-                    convos.push(convo)
-                    count++;
-                    if(count >= limit){
-                        return serve (null, {convos: convos})
-                    }
-                })
-            }
-        }
-    )
+    db.query(query, [], (error, results) => {
+        processFeed(error, results, batch, serve)
+    })
 }
 
 function getFeedByUser(username, batch, serve) {
-    /*
-    //works with less code but extra query
-    const query = 'SELECT user_id FROM users WHERE username = $1'
-    db.query(query,[username], (error, results) => {
-        if(error){
-            console.error('select user_id failed', error)
-            return serve (true, 'select user_id failed')
-        } else if(results.rowCount < 1) {
-            return serve(true, 'Invalid username')
-        } else {
-            getFeedById(results[0]['user_id'], batch, serve)
-        }
-    })
-    */
-
-    //get all data in one query
     const query = 'SELECT convo.* FROM users ' +
     'INNER JOIN contributor ON users.user_id = contributor.contributor_id ' + 
     'INNER JOIN convo ON convo.convo_id = contributor.convo_id ' +
     'WHERE users.username = $1 AND contributor.accepted_at IS NOT NULL ' +
     'ORDER BY votes DESC'
-    db.query(
-        query, [username], (error, results) => {
-            if(error){
-                console.error('select feed failed', error)
-                return serve (true, "select feed failed")
-            } else if (results.rowCount < 1) {
-                console.error('Feed is empty')
-                return serve (true, 'Feed is empty')
-            }
-
-            convos = []
-
-            let i = (batch - 1) * 10
-            let count = i
-            const limit = (results.rowCount < i + 10) ? results.rowCount : 10 + i
-            for(i; i < limit; i++){
-                let convo = results.rows[i]
-                const last_post = moment.utc(convo.last_post_at, moment.ISO_8601)
-                convo.age = moment(last_post).fromNow()
-
-                getContributors(convo.convo_id, (error, response) => {
-                    if(error){
-                        return serve (error, response)
-                    }
-                    convo.contributors = stringifyContributors(response)
-                    convo.contributorCount = response.length
-                    convos.push(convo)
-                    count++;
-                    if(count >= limit){
-                        return serve (null, {convos: convos})
-                    }
-                })
-            }
-        }
-    )
+    db.query(query, [username], (error, results) => {
+        processFeed(error, results, batch, serve)
+    })
 }
 
 function getFeedById(user_id, batch, serve) {
@@ -112,41 +32,9 @@ function getFeedById(user_id, batch, serve) {
     'INNER JOIN convo ON convo.convo_id = contributor.convo_id ' +
     'WHERE contributor.contributor_id = $1 AND contributor.accepted_at IS NOT NULL ' +
     'ORDER BY votes DESC'
-    db.query(
-        query, [user_id], (error, results) => {
-            if(error){
-                console.error('select feed failed', error)
-                return serve (true, "select feed failed")
-            } else if (results.rowCount < 1) {
-                console.error('Feed is empty')
-                return serve (true, 'Feed is empty')
-            }
-
-            convos = []
-
-            let i = (batch - 1) * 10
-            let count = i
-            const limit = (results.rowCount < i + 10) ? results.rowCount : 10 + i
-            for(i; i < limit; i++){
-                let convo = results.rows[i]
-                const last_post = moment.utc(convo.last_post_at, moment.ISO_8601)
-                convo.age = moment(last_post).fromNow()
-
-                getContributors(convo.convo_id, (error, response) => {
-                    if(error){
-                        return serve (error, response)
-                    }
-                    convo.contributors = stringifyContributors(response)
-                    convo.contributorCount = response.length
-                    convos.push(convo)
-                    count++;
-                    if(count >= limit){
-                        return serve (null, {convos: convos})
-                    }
-                })
-            }
-        }
-    )
+    db.query(query, [user_id], (error, results) => {
+        processFeed(error, results, batch, serve)
+    })
 }
 
 /*
@@ -154,6 +42,40 @@ function getFeedByTag(tag, batch, serve){
     return serve (null, null)
 }
 */
+
+function processFeed(error, results, batch, serve){
+    if(error){
+        console.error('select feed failed', error)
+        return serve (true, "select feed failed")
+    } else if (results.rowCount < 1) {
+        console.error('Feed is empty')
+        return serve (true, 'Feed is empty')
+    }
+
+    convos = []
+
+    let i = (batch - 1) * 10
+    let count = i
+    const limit = (results.rowCount < i + 10) ? results.rowCount : 10 + i
+    for(i; i < limit; i++){
+        let convo = results.rows[i]
+        const last_post = moment.utc(convo.last_post_at, moment.ISO_8601)
+        convo.age = moment(last_post).fromNow()
+
+        getContributors(convo.convo_id, (error, response) => {
+            if(error){
+                return serve (error, response)
+            }
+            convo.contributors = stringifyContributors(response)
+            convo.contributorCount = response.length
+            convos.push(convo)
+            count++;
+            if(count >= limit){
+                return serve (null, {convos: convos})
+            }
+        })
+    }
+}
 
 function stringifyContributors (contributors){
     const count = contributors.length
