b6f3f22f21241a7bb2cf7952a9f2c4f0c2ae6451
Judah <jk5445@nyu.edu>
2019-09-23 20:23:34 -0400

    get and post comment

--- db/comment/index.js
+++ db/comment/index.js
@@ -1,26 +1,58 @@
+const db = require('./query')
+const {authenticate} = require('../auth')
+const validate = require('validator')
+
 //COMMENTS
 
 modules.exports = app => {
     //get comments
     app.get('/api/v1/comment/:convo_id', (request, response) => {
-        //TODO: implement method
+        const convo_id = request.params.convo_id
+
+        if(!validate.isUUID(convo_id, 4)) {
+            return response.status(400).json({ message: "Invalid convo_id" })
+        }
+
+        db.getComment(convo_id, (err, res) => {
+            if(err)
+                return response.status(400).json({ message: res })
+            return response.json(res)
+        })
     })
 
     //make base comment
     //auth
-    app.post('/api/v1/comment/:convo_id', (request, response) => {
-        //TODO: implement method
+    app.post('/api/v1/comment/:convo_id', authenticate, (request, response) => {
+        const user_id = request.body.user_id
+        const convo_id = request.params.convo_id
+        const comment = response.body.comment
+
+        if(!validate.isUUID(convo_id, 4)) {
+            return response.status(400).json({ message: "Invalid convo_id" })
+        }
+
+        if(!comment || !validate.isLength(comment, {min: 1, max: undefined})) {
+            return response.status(400).send({ message: "Comment must contain at least 1 characters" })
+        }
+
+        db.createComment(convo_id, comment, (err, res) => {
+            if(err)
+                return response.status(400).json({ message: res })
+            return response.status(201).json({ message: res })
+        })
     })
 
     //comment as reply to prev comment
     //auth
     app.post('/api/v1/comment/:convo_id/:comment_id', (request, response) => {
         //TODO: implement method
+        response.json({ message: "not yet implemented"})
     })
 
     //vote on comment
     //auth
     app.put('/api/v1/comment/vote/:comment_id/:vote', (request, response) => {
         //TODO: implement method
+        response.json({ message: "not yet implemented"})
     })
 }
--- db/comment/query.js
+++ db/comment/query.js
@@ -1,2 +1,32 @@
 const db = require('../queries')
 
+module.exports = {
+    createComment,
+    getComment
+}
+
+function createComment(convo_id, user_id, comment, serve){
+    const query = 'INSERT INTO comment (convo_id, user_id, comment) VALUES ($1, $2, $3)'
+    db.query(query, [convo_id, user_id, comment], (error, results) => {
+        if(error) {
+            console.error('Insert comment failed', error)
+		    return serve (true, 'Insert comment failed')
+        }
+        return serve(null, "success")
+    })
+}
+
+function getComment(convo_id, serve){
+    const query = 'SELECT * FROM comment WHERE convo_id = $1 ORDER BY comment_at DESC'
+    db.query(query, [convo_id], (error, results) => {
+        if(error) {
+            console.error('Select comments failed', error)
+            return serve(true, 'Select comments failed')
+        } else if(results.rowCount < 1) {
+            return serve(true, 'No comments or non-existent convo_id')
+        }
+
+        return serve(null, { comments: results })
+    })
+}
+
