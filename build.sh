c47eb66de8074a697748c445b428ef7b0689fdd4
Jack Hedaya <jackehedaya@gmail.com>
2019-09-26 22:35:43 -0400

    fix react-scripts build issue

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 echo "Pulling client..."
-git clone https://github.com/jackHedaya/saloon-client react-build
+git clone https://github.com/jackHedaya/saloon-client __client-codebase
 echo "Pull complete"
 
-echo "Building Client"
-cd react-build/
-npm run build
+echo "Building client..."
+cd __client-codebase/
+npm install --only=dev && npm install && npm run build
 echo "Build complete"
+
+echo "Moving build..."
+mv build/ ../react-build
+echo "Move complete"
+
+echo "Removing client codebase..."
+cd ../
+rm -rf __client-codebase
+echo "Removed client codebase"
