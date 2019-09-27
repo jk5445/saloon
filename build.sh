74a36279c4de226fd3440cfa9aacec64a9464eca
Jack Hedaya <jackehedaya@gmail.com>
2019-09-27 02:49:56 -0400

    prevent client from being rebuilt if its up to date

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+REMOTE_HASH=$(git ls-remote https://github.com/jackHedaya/saloon-client.git HEAD | awk '{ print $1}')
+STORE_FILE='VERSION_HASH'
+
+
+if [[ -f "react-build/$STORE_FILE" && $(< react-build/$STORE_FILE) == "$REMOTE_HASH" ]]; then
+  echo "Client up to date. Exiting..."
+  exit
+fi
+
 echo "Pulling client..."
 git clone https://github.com/jackHedaya/saloon-client __client-codebase
 echo "Pull complete"
@@ -9,6 +18,7 @@ echo "Build complete"
 
 echo "Moving build..."
 mv build/ ../react-build
+echo "$REMOTE_HASH" > "../react-build/$STORE_FILE"
 echo "Move complete"
 
 echo "Removing client codebase..."
