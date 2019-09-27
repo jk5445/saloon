0b624f52f30aaca0071d604a90b7c4dca380375f
Jack Hedaya <jackehedaya@gmail.com>
2019-09-26 22:26:14 -0400

    added build logs

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+echo "Pulling client..."
 git clone https://github.com/jackHedaya/saloon-client react-build
+echo "Pull complete"
+
+echo "Building Client"
 cd react-build/
 npm run build
+echo "Build complete"
