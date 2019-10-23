aa07d30dcce7510c6870100ad93a30a9ee946870
Jack Hedaya <jackehedaya@gmail.com>
2019-10-22 22:02:22 -0400

    updated react-client

---
+++ react-build/precache-manifest.2bcfcad10dc8a79a97d69f9ba9f573fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a092a9fa0631ff065ebf1fa24a4e0de9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c9d894e142e4214b5d8",
+    "url": "/static/css/2.2671185e.chunk.css"
+  },
+  {
+    "revision": "bcb5204fb767b675e229",
+    "url": "/static/css/main.02c8b1b2.chunk.css"
+  },
+  {
+    "revision": "9c9d894e142e4214b5d8",
+    "url": "/static/js/2.8d3c1548.chunk.js"
+  },
+  {
+    "revision": "bcb5204fb767b675e229",
+    "url": "/static/js/main.de1f916f.chunk.js"
+  },
+  {
+    "revision": "d7f9edecb4578445a7c6",
+    "url": "/static/js/runtime-main.cf11bef3.js"
+  }
+]);
