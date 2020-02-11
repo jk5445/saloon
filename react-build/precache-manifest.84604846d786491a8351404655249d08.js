3352f3918a94b90c7160162a3359f859e9a64d79
Judah <jk5445@nyu.edu>
2020-02-11 14:23:37 -0500

    new build

---
+++ react-build/precache-manifest.84604846d786491a8351404655249d08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63dd44f071e98b2bb04e6c58b307038c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04af78ad90115c741bcb",
+    "url": "/static/css/2.0976cf5b.chunk.css"
+  },
+  {
+    "revision": "1965641401c860ad3d78",
+    "url": "/static/css/main.ef9ffecf.chunk.css"
+  },
+  {
+    "revision": "04af78ad90115c741bcb",
+    "url": "/static/js/2.d502c1b4.chunk.js"
+  },
+  {
+    "revision": "859e9f5f2979aa4dd183c8c0895b634a",
+    "url": "/static/js/2.d502c1b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1965641401c860ad3d78",
+    "url": "/static/js/main.ba7bd082.chunk.js"
+  },
+  {
+    "revision": "8d044afe48ae5f239e37",
+    "url": "/static/js/runtime-main.3febb3e4.js"
+  }
+]);
