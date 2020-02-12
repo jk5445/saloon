381bbee43c9ddb1b5f1b7981fd05e6b92b1f8258
jackHedaya <jackhedaya@Gladyss-MacBook-Air.local>
2020-02-11 22:45:28 -0500

    update build

---
+++ react-build/precache-manifest.829a3c145d63080b243ad413c53701da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b20dd0bd52c5e1d73bd3108952d3d91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "131951868b1e92b42ca0",
+    "url": "/static/css/2.f4a3ddda.chunk.css"
+  },
+  {
+    "revision": "b7f21c3fffd7630a698e",
+    "url": "/static/css/main.7021013f.chunk.css"
+  },
+  {
+    "revision": "131951868b1e92b42ca0",
+    "url": "/static/js/2.2bae41b7.chunk.js"
+  },
+  {
+    "revision": "859e9f5f2979aa4dd183c8c0895b634a",
+    "url": "/static/js/2.2bae41b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f21c3fffd7630a698e",
+    "url": "/static/js/main.7f86c4dd.chunk.js"
+  },
+  {
+    "revision": "8d044afe48ae5f239e37",
+    "url": "/static/js/runtime-main.3febb3e4.js"
+  }
+]);
--- react-build/service-worker.js
+++ react-build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d6e9b4026d22ed9bea47781400b430a.js"
+  "/precache-manifest.829a3c145d63080b243ad413c53701da.js"
 );
 
 self.addEventListener('message', (event) => {
