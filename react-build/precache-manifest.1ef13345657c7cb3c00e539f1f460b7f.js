4cd10b8793eeb4b2f8e43b01e63cfbd354a6b777
jackHedaya <jackhedaya@Gladyss-MacBook-Air.local>
2020-02-11 22:17:52 -0500

    update build

---
+++ react-build/precache-manifest.1ef13345657c7cb3c00e539f1f460b7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f8a96c838141e249ff9a51fdd88cee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "131951868b1e92b42ca0",
+    "url": "/static/css/2.f4a3ddda.chunk.css"
+  },
+  {
+    "revision": "973373a2c2e16e3b7e0a",
+    "url": "/static/css/main.1ae2ab33.chunk.css"
+  },
+  {
+    "revision": "131951868b1e92b42ca0",
+    "url": "/static/js/2.2bae41b7.chunk.js"
+  },
+  {
+    "revision": "859e9f5f2979aa4dd183c8c0895b634a",
+    "url": "/static/js/2.2bae41b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973373a2c2e16e3b7e0a",
+    "url": "/static/js/main.bfd417ff.chunk.js"
+  },
+  {
+    "revision": "8d044afe48ae5f239e37",
+    "url": "/static/js/runtime-main.3febb3e4.js"
+  }
+]);
--- react-build/service-worker.js
+++ react-build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.84604846d786491a8351404655249d08.js"
+  "/precache-manifest.1ef13345657c7cb3c00e539f1f460b7f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
